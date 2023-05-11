ecce2155551ff619651fb5d33d408d1bfca1f01e
qkrwjdtn1236 <qkrwjdtn1236@gmail.com>
2023-05-11 14:12:03 +0900

    data prepro code perform fix

--- data_prepro.py
+++ data_prepro.py
@@ -24,13 +24,13 @@ fillnaBehind 함수
 데이터의 평균으로 바꾸어줍니다.
 '''
 def fillnaBehind(Data:pd.DataFrame):
-    nullBoolean = Data['구미 혁신도시배수지 유출유량 적산차'].isnull()
+    nullBoolean = Data['구미 혁신도시배수지 유출유량 적산차'] == 0
     nullData = Data[nullBoolean].index
     # print(nullData)
     for i in nullData:
         # if Data.loc[i-1,'구미 혁신도시배수지 유출유량 적산차'] == np.NaN:
         #     Data.loc[i,'구미 혁신도시배수지 유출유량 적산차'] = Data.loc[i+1,'구미 혁신도시배수지 유출유량 적산차']
-        if Data.loc[i+1,'구미 혁신도시배수지 유출유량 적산차'] == np.NaN:
+        if Data.loc[i+1,'구미 혁신도시배수지 유출유량 적산차'] == 0:
             Data.loc[i,'구미 혁신도시배수지 유출유량 적산차'] = Data.loc[i-1,'구미 혁신도시배수지 유출유량 적산차']
         else:
             Data.loc[i,'구미 혁신도시배수지 유출유량 적산차'] = \
@@ -38,7 +38,7 @@ def fillnaBehind(Data:pd.DataFrame):
                 Data.loc[i+1,'구미 혁신도시배수지 유출유량 적산차'])//2.0
     
     print(nullData)
-    Data = Data.fillna(method='pad')
+    
 
     print(Data.isnull().sum())
 
@@ -56,39 +56,60 @@ def fillZero(Data:pd.DataFrame):
     for i in zeroIndex:
         if len(indexlist) == 0:
             indexlist.append(i)
-            prev = zeroIndex[0]
-        elif prev + 1 == i: # 연속적이냐?
-            sum+=Data.loc[i]['구미 혁신도시배수지 유출유량 적산차']
             
+        elif prev + 1 == i: # 연속적이냐?
             indexlist.append(i)
+            
         else: # 연속이 아닐때
-            pass
+            total = Data.loc[prev+1]['구미 혁신도시배수지 유출유량 적산차']
+            print(total)
+            indexlist.append(prev+1)
+            cnt = len(indexlist)
+            Data.loc[np.array(indexlist)] = total / cnt
+            indexlist.clear()
+        prev = i
+
+    print(Data[Data['구미 혁신도시배수지 유출유량 적산차']==0].index)
+    return Data
+'''
+fillprevValue(Data:pd.Dataframe)
+na값들은 이전의 데이터로 덮어 씌우는 함수입니다.
+'''
+def fillprevValue(Data:pd.DataFrame):
+    
+    return Data.fillna(method='pad')
 
 '''
 outlierDataToNan 함수
 이 함수는 이상치 값(값이 너무 높은 값만)만 NaN으로
+
+수정 5/11 : 너무 수치가 높은 값들은 데이터 형태에 따라서 부적합하다는 판단으로 극대값은 전처리 안함
+단, 0보다 작은 값과 Nan 값은 다 0으로 처리
 '''
 
-def outlierDataToNan(Data:pd.DataFrame,thres = 3.0):
-    # 이 부분은 조심히 다뤄야 함.(fect. 교수)
-    q1=Data['구미 혁신도시배수지 유출유량 적산차'].quantile(0.25)
-    q2=Data['구미 혁신도시배수지 유출유량 적산차'].quantile(0.5)
-    q3=Data['구미 혁신도시배수지 유출유량 적산차'].quantile(0.75)
+def outlierDataToNan(Data:pd.DataFrame,low:bool = False,high:bool = False,thres = 3.0):
 
-    iqr = q3-q1
+    if high:
+        # 이 부분은 조심히 다뤄야 함.(fect. 교수)
+        q1=Data['구미 혁신도시배수지 유출유량 적산차'].quantile(0.25)
+        q2=Data['구미 혁신도시배수지 유출유량 적산차'].quantile(0.5)
+        q3=Data['구미 혁신도시배수지 유출유량 적산차'].quantile(0.75)
 
-    remove = Data['구미 혁신도시배수지 유출유량 적산차']> q3+thres*iqr
-    Data[remove] = np.NaN
+        iqr = q3-q1
 
-    Data[Data['구미 혁신도시배수지 유출유량 적산차']<0] = np.NaN
+        remove = Data['구미 혁신도시배수지 유출유량 적산차']> q3+thres*iqr
+        Data[remove] = Data['구미 혁신도시배수지 유출유량 적산차'].quantile(0.9)
+
+    # Data[Data['구미 혁신도시배수지 유출유량 적산차']<0] = np.NaN
+
+    if low:
+        Data[Data['구미 혁신도시배수지 유출유량 적산차']<=0]= 0
+        Data[Data['구미 혁신도시배수지 유출유량 적산차']<np.NaN] = 0
 
     return Data
-
-
-
 '''
 XDataToXAndYSeq(Data,step)
-학습하
+시계열 데이터 X,Y 변수로 변환해주는 값입니다, 기본 step은 24로 지정되어 있습니다.
 
 '''
 def XDataToXAndYSeq(Data:pd.DataFrame,step = 24):
